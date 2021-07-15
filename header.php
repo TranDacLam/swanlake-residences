c09076fff05ba6f58e7107c5dc577ec885a6e057
TranDacLam <tdlam2105@gmail.com>
2021-07-15 18:25:25 +0700

    add google tagmanager

--- header.php
+++ header.php
@@ -24,11 +24,11 @@
     </script>
 
     <!-- Google Tag Manager -->
-    <!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-N32MKLM');</script> -->
+    })(window,document,'script','dataLayer','GTM-PXJGFQT');</script>
     <!-- End Google Tag Manager -->
 
     <!-- Admicro Tag Manager -->
@@ -53,8 +53,6 @@
 
 <body <?php body_class(); ?>>
     <!-- Google Tag Manager (noscript) -->
-    <noscript>
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N32MKLM"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe>
-    </noscript>
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PXJGFQT"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
